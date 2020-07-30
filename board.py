d1295f93e43ea6858f29c2a59b1becf62033450d
arthurvergacas <arthurvdm@gmail.com>
2020-07-30 20:07:50 -0300

    add board generator and condensed both classes

--- board.py
+++ board.py
@@ -1,10 +1,9 @@
 import numpy as np
 import random
 import timeit
-from game import Game
 
 
-class Board(Game):
+class Board:
     """
     Class object to handle a single board as a game.
     Inherits Game
@@ -13,8 +12,45 @@ class Board(Game):
         board (array): A 2 dimensional array (9, 9) representing the board. Empty spaces are represented with zeros. 
     """
 
-    def __init__(self, board):
-        self.board = board
+    def __init__(self, board=None):
+        self.board = self.createBoard(30) if board is None else board
+
+    def isPossible(self, board, y, x, n):
+        """
+        Checks if a given number in given positions on a given board is possible
+
+        Args:
+            board (array): A 2 dimensional array that contains the board
+            y (int): The line position
+            x (inte): The column postion
+            n (int): The number from 1 to 9 that check against the board in the postion
+
+        Returns:
+            boolean: True if it is possible, otherwise returns False
+        """
+
+        # check in line
+        for i in board[y]:
+            if i == n:
+                return False
+
+        # check in column
+        for i in range(9):
+            if board[i][x] == n:
+                return False
+
+        # check in square
+        # number between 0 and 2 to indicate the x square
+        squareX = (x // 3) * 3
+        # same for y square
+        squareY = (y // 3) * 3
+
+        for i in range(squareY, squareY+3):
+            for j in range(squareX, squareX+3):
+                if board[i][j] == n:
+                    return False
+
+        return True
 
     def solveBoard(self):
         """
@@ -63,6 +99,45 @@ class Board(Game):
         # return the solved board
         return new.tolist()
 
+    def createBoard(self, starter_nums):
+
+        board = []
+
+        def create():
+            new = np.zeros((9, 9), dtype=int).tolist()
+
+            for i in range(starter_nums):
+                # pick random row, collumn and number to fill a cell
+                row = np.random.randint(0, 9)
+                col = np.random.randint(0, 9)
+                num = np.random.randint(1, 10)
+
+                # while the current spot is not avilable, keep picking
+                while (not self.isPossible(new, row, col, num) or new[row][col] != 0):
+                    row = np.random.randint(0, 9)
+                    col = np.random.randint(0, 9)
+                    num = np.random.randint(1, 10)
+
+                # assign the number to the cell
+                new[row][col] = num
+            return new
+
+        # check if game is possible and if not, redo the process
+        possible = False
+        tests = 0
+        while (not possible):
+            try:
+                self.board = create()
+                board = self.board[:]
+                self.solveBoard()
+                possible = True
+            except AttributeError:
+                tests += 1
+                print(tests, end="\r")
+                possible = False
+
+        return board
+
 
 if __name__ == "__main__":
     grid = [[4, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -75,10 +150,6 @@ if __name__ == "__main__":
             [7, 0, 0, 0, 0, 5, 0, 6, 2],
             [0, 0, 3, 7, 0, 0, 0, 0, 0]]
 
-    game = Board(grid)
-
-    start = timeit.default_timer()
-    print(game.solveBoard())
-    end = timeit.default_timer()
-    elapsedTime = str("{:.6f}".format(end - start))
-    print(elapsedTime)
+    game = Board()
+    print(np.array(game.board))
+    print(np.array(game.solveBoard()))
