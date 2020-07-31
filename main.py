4db24ebb55545c78b3168bc7ee45c4c5b9c3ff44
arthurvergacas <arthurvdm@gmail.com>
2020-07-31 13:49:16 -0300

    added grid

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from board import Board
 class Main:
 
     def __init__(self):
-        self.currentBoard = Board()
+        self.currentBoard = Board(difficulty=2)
 
         self.HEIGHT = 450
         self.WIDTH = 450
@@ -20,6 +20,49 @@ class Main:
         self.running = True
         self.clock = pygame.time.Clock()
 
+    def drawGrid(self):
+        resY = int(self.HEIGHT / 9)
+        resX = int(self.WIDTH / 9)
+
+        # border lines
+        pygame.draw.line(self.backgound, (0, 0, 0),
+                         (0, 0), (self.WIDTH, 0))
+        pygame.draw.line(self.backgound, (0, 0, 0),
+                         (0, self.HEIGHT), (self.WIDTH, self.HEIGHT))
+        pygame.draw.line(self.backgound, (0, 0, 0),
+                         (0, 0), (0, self.HEIGHT))
+        pygame.draw.line(self.backgound, (0, 0, 0),
+                         (self.WIDTH, 0), (self.WIDTH, self.HEIGHT))
+
+        # 8 because I want to draw the border lines ou of the loop
+        for i in range(8):
+            # horizontal lines
+            y = i * resY + resY
+            # vertical lines
+            x = i * resX + resX
+            if (i + 1) % 3 == 0:
+                # horizontal bold
+                pygame.draw.line(self.backgound, (0, 0, 0),
+                                 (0, y),
+                                 (self.WIDTH, y), 3)
+                # vertical bold
+                pygame.draw.line(self.backgound, (0, 0, 0),
+                                 (x, 0),
+                                 (x, self.HEIGHT), 3)
+            else:
+                # horizontal
+                pygame.draw.line(self.backgound, (0, 0, 0),
+                                 (0, y),
+                                 (self.WIDTH, y))
+                # vertical
+                pygame.draw.line(self.backgound, (0, 0, 0),
+                                 (x, 0),
+                                 (x, self.HEIGHT))
+
+            pygame.draw.line(self.backgound, (0, 0, 0),
+                             (x, 0),
+                             (x, self.HEIGHT))
+
     def run(self):
         while self.running:
             self.clock.tick(30)
@@ -29,7 +72,7 @@ class Main:
                     self.running = False
 
             self.screen.blit(self.backgound, (0, 0))
-
+            self.drawGrid()
             pygame.display.update()
 
 
